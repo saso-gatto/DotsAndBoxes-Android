a880e2332f8f36d58e0ea634ae9d966f53426bd3
davyde8 <davyde8@gmail.com>
2021-04-15 15:24:41 +0200

    Aggiunta pagina Iniziale (start)

---
+++ app/src/main/java/it/dotsandboxes/Activity_Start.java
@@ -0,0 +1,22 @@
+package it.dotsandboxes;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Activity_Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__start);
+
+
+    }
+}
