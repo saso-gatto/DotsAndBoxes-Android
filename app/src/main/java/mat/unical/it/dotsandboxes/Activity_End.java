770215a6c8428f36648b69cb02df24dee975928f
Saso <salvatore.gatto99@gmail.com>
2021-05-14 11:27:56 +0200

    Aggiunta Activty-End

--- app/src/main/java/mat/unical/it/dotsandboxes/Activity_End.java
+++ app/src/main/java/mat/unical/it/dotsandboxes/Activity_End.java
@@ -1,14 +1,14 @@
 package mat.unical.it.dotsandboxes;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Activity_Info extends AppCompatActivity {
+public class Activity_End extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
